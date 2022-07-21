815e4284422dbf34c7291d578c772f0b9942aca1
Liam Chivers <liamchivers2k9@hotmail.co.uk>
2022-07-21 03:27:10 +0100

    Updated script.js with improved logic for game function

--- script.js
+++ script.js
@@ -10,50 +10,34 @@ function getComputerChoice(){
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
-
-
 function playRound(playerSelection, computerSelection){
 
-    if(playerScore < 5 && computerScore < 5){
-        if(playerSelection === computerSelection){
-            return "it's a draw! Try again!"
-        } else if(playerSelection === 'paper' && computerSelection === 'rock' || 
-                    playerSelection === 'rock' && computerSelection === 'scissors' ||
-                    playerSelection === 'scissors' && computerSelection === 'paper'){
-                        playerScore += 1
-                        return `You win! ${playerSelection} beats ${computerSelection}.`
-        }else {
-            computerScore += 1
-            return `You lose! ${computerSelection} beats ${playerSelection}.`      
-    
-        }
-    } else if(playerScore === 5){
-        return 'Congrats! You beat the computer'
+    if(playerSelection === computerSelection){
+        return "it's a draw! Try again!"
+    } else if(playerSelection === 'paper' && computerSelection === 'rock' || 
+        playerSelection === 'rock' && computerSelection === 'scissors' ||
+        playerSelection === 'scissors' && computerSelection === 'paper'){
+            playerScore += 1
+            return `You win! ${playerSelection} beats ${computerSelection}.`
     } else {
-        return 'Thats unfortunate. The computer beat you!'
+        computerScore += 1
+        return `You lose! ${computerSelection} beats ${playerSelection}.`      
     }
-
-  
-
 }
 
-
-
 function game(){
-
-
     gameBtns.forEach(btn => {
         btn.addEventListener('click', () => {
+            if(playerScore >= 5 || computerScore >= 5){
+                return 
+            }
             const player = btn.textContent
             const computer = getComputerChoice()
 
             gameInfo.textContent = playRound(player, computer)
             scores.textContent = `Player Score: ${playerScore} VS Computer Score: ${computerScore}`
-
         })
     })
 }
 
 game()
-
-
